5fbcf28d81bd920b007fafa2e17ba7c759f3791b
muigai.esthern@gmail.com <muigai.esthern@gmail.com>
2024-03-30 18:54:38 +0300

    modified:   E-commerce fashion website.css 	modified:   E-commerce fashion website.html 	modified:   script.js

--- script.js
+++ script.js
@@ -31,6 +31,53 @@ themeToggler.onclick = () =>{
     }
 }
 
+document.querySelectorAll(".small-image-1").forEach(images => {
+    images.onclick = () =>{
+        document.querySelector('.big-image-1').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll(".small-image-2").forEach(images => {
+    images.onclick = () =>{
+        document.querySelector('.big-image-2').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll(".small-image-3").forEach(images => {
+    images.onclick = () =>{
+        document.querySelector('.big-image-3').src = images.getAttribute('src');
+    }
+});
+
+let countDate = new Date("Mar, 2024 00:00:00").getTime();
+
+function countDown(){
+
+    let now = new Date().getTime();
+    gap = now - countDate;
+
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hours = minutes * 60;
+    let day = hours * 24;
+    
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hours));
+    let m = Math.floor((gap % (hours)) / (minutes));
+    let s = Math.floor((gap % (minutes)) / (seconds));
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown()
+},1000);
+
+
 var swiper = new Swiper(".product-slider",{
    slidesPerView: 3,
    loop: true,
@@ -57,5 +104,27 @@ var swiper = new Swiper(".product-slider",{
         slidesPerView: 3,
     },
    },
-
 });
+var swiper = new Swiper(".review-slider",{
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 10,
+    autoplay: {
+     delay: 4000,
+     disableOnInteraction: false,
+    },
+    breakpoints: {
+     0: {
+         slidesPerView: 1,
+     },
+     550: {
+         slidesPerView: 2,
+     },
+     800:{
+         slidesPerView: 3,
+     },
+     1000:{
+         slidesPerView: 3,
+     },
+    },
+ });
